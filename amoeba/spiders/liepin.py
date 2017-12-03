9c107a5e7816ebc5eba203b82ecbbb95006ad886
lijingping <47354075@qq.com>
2017-12-04 00:16:46 +0800

    add flask run envirnoment

--- amoeba/spiders/liepin.py
+++ amoeba/spiders/liepin.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-from selenium import webdriver
 
 class LiepinSpider(scrapy.Spider):
     name = 'liepin'
@@ -9,10 +8,4 @@ class LiepinSpider(scrapy.Spider):
     base_url = ['https://passport.liepin.com/e/account#sfrom=click-pc_homepage-front_navigation-ecomphr_new']
 
     def parse(self, response):
-    	driver = webdriver.Chrome()
-	driver.get(self.base_url)
-
-	#driver.find_element_by_id("kw").click()
-	#driver.find_element_by_id("kw").send_keys("123")
-	#driver.find_element_by_id("su").click()
-	pass
+    	pass
